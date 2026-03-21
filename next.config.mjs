3119df41ef3db4196079fb68849323ce281b8c04
tutionpoint <tutionpoint.app@gmail.com>
2026-03-21 18:22:42 +0530

    before merging to main

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,26 @@
 /** @type {import('next').NextConfig} */
+
+
 const nextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        has: [
+          {
+            type: 'host',
+            value: 'www.8cpccalculator.com',
+          },
+        ],
+        destination: 'https://8cpccalculator.com/:path*',
+        permanent: true,
+      },
+    ];
+  },
 };
+
+export default nextConfig;
+
+
 
 export default nextConfig;
