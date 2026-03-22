7d73babbfb2c6c29cc84498df2b5a2d81f92c83e
tutionpoint <tutionpoint.app@gmail.com>
2026-03-22 09:47:57 +0530

    sitemap updated

--- app/sitemap.js
+++ app/sitemap.js
@@ -1,12 +1,13 @@
 export default function sitemap() {
   return [
-    {
-      url: "https://8cpccalculator.com",
-      lastModified: new Date(),
-    },
-    {
-      url: "https://8cpccalculator.com/arrear-calculator",
-      lastModified: new Date(),
-    }
+    { url: "https://8cpccalculator.com" },
+    { url: "https://8cpccalculator.com/8th-cpc-arrear" },
+    { url: "https://8cpccalculator.com/8th-cpc-pension-calculator" },
+    { url: "https://8cpccalculator.com/7th-cpc-calculator" },
+    { url: "https://8cpccalculator.com/7th-cpc-pay-matrix" },
+    { url: "https://8cpccalculator.com/7th-cpc-pension-calculator" },
+    { url: "https://8cpccalculator.com/8th-cpc-pay-matrix" },
+    { url: "https://8cpccalculator.com/8th-cpc-pension-arrear" },
+    { url: "https://8cpccalculator.com/pay-fixation-calculator" },
   ];
 }
