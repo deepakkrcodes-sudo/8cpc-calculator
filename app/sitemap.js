477a8baecf6968425321b67c202deda38da97049
unknown <deepak.kr.codes@gmail.com>
2026-04-27 22:32:24 +0530

    updated sitemap after adding all ltc pages

--- app/sitemap.js
+++ app/sitemap.js
@@ -9,6 +9,10 @@ export default function sitemap() {
     { url: "https://8cpccalculator.com/8th-cpc-pay-matrix" },
     { url: "https://8cpccalculator.com/8th-cpc-pension-arrear" },
     { url: "https://8cpccalculator.com/pay-fixation-calculator" },
+    { url: "https://8cpccalculator.com/ltc-air-travel-rules" },
+    { url: "https://8cpccalculator.com/ltc-entitlement" },
+    { url: "https://8cpccalculator.com/ltc-faq" },
     { url: "https://8cpccalculator.com/ltc-planner" },
+    { url: "https://8cpccalculator.com/ltc-rules-central-government" },
   ];
 }
