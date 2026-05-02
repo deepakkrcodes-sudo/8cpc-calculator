f3d1f4f48d8ba4727929ea72083d27abb45c6e5a
unknown <deepak.kr.codes@gmail.com>
2026-05-02 15:14:27 +0530

    updated sitemap after adding the insights pages

--- app/sitemap.js
+++ app/sitemap.js
@@ -14,5 +14,12 @@ export default function sitemap() {
     { url: "https://8cpccalculator.com/ltc-faq" },
     { url: "https://8cpccalculator.com/ltc-planner" },
     { url: "https://8cpccalculator.com/ltc-rules-central-government" },
+    { url: "https://8cpccalculator.com/8th-cpc/insights/history-of-pay-commission-india" },
+    { url: "https://8cpccalculator.com/8th-cpc/insights/most-probable-fitment-factor-8th-cpc" },
+    { url: "https://8cpccalculator.com/8th-cpc/insights/factors-affecting-8th-pay-commission" },
+    { url: "https://8cpccalculator.com/8th-cpc/insights/8th-cpc-questions-analysis" },
+    { url: "https://8cpccalculator.com/8th-cpc/insights" },
+
+
   ];
 }
